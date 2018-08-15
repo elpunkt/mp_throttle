bea51ca3162b7687cd7f1cfc701d3edebe5d23eb
elpunkt <lucas@langholf.de>
2018-08-15 23:11:31 +0200

    introduced single-source version control.

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 import setuptools
+import re
+import os
 
+def find_version_author_mail():
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, "mp_throttle/__init__.py"), 'r') as fp:
+        version_file = fp.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    author_match = re.search(r"^__author__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    author, mail = re.split(r'<',author_match.group(1))
+    return version_match.group(1), author.rstrip(), re.sub('>', '', mail)
+
+version, author, mail = find_version_author_mail()
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="mp_throttle",
-    version="0.0.1",
-    author="Lucas Langholf",
-    author_email="lucas@elpunkt.eu",
+    version=__version__,
+    author=author,
+    author_email= mail,
     description="Package to monitor and throttle multiprocessing processes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
